44f71ace7cf85b1551ccdb36ef850e05e086167b
Branislav Kontur <bkontur@gmail.com>
2025-10-01 12:19:30 +0200

    Added while loop

--- integration-tests/bridges/tests/0002-polkadot-bulletin/check-authorized-upgrade.js
+++ integration-tests/bridges/tests/0002-polkadot-bulletin/check-authorized-upgrade.js
@@ -4,29 +4,27 @@ async function run(nodeName, networkInfo, args) {
 
     const expectedCodeHash = args.codeHash;
     
-    console.log(" Checking for authorized upgrade...");
-    const authorizedUpgrade = await api.query.system.authorizedUpgrade();
+    console.log(" Checking for authorized upgrade... expectedCodeHash: " + expectedCodeHash);
+    while (true) {
+        const authorizedUpgrade = await api.query.system.authorizedUpgrade();
 
-    if (authorizedUpgrade.isSome) {
-        const authorizedCodeHash = authorizedUpgrade.unwrap();
-        console.log(" Found authorized upgrade with codeHash: " + authorizedCodeHash);
-        
-        if (expectedCodeHash && expectedCodeHash === authorizedCodeHash.toString()) {
-            console.log(" Ok - Expected code hash matched: " + expectedCodeHash);
-            return true;
-        } else if (expectedCodeHash && expectedCodeHash !== authorizedCodeHash.toString()) {
-            throw new Error("Wrong authorized code hash! Expected: " + expectedCodeHash + " actual: " + authorizedCodeHash.toString());
-        } else {
-            // if no expected hash provided, return true
-            console.log(" Current authorized code hash: " + authorizedCodeHash.toString());
+        if (expectedCodeHash && authorizedUpgrade.isSome) {
+            const authorizedCodeHash = authorizedUpgrade.unwrap();
+            console.log(" Found authorized upgrade with codeHash: " + authorizedCodeHash);
+
+            if (expectedCodeHash === authorizedCodeHash.toString()) {
+                console.log(" Ok - Expected code hash matched: " + expectedCodeHash);
+                return true;
+            }
+        }
+
+        if (!expectedCodeHash && authorizedUpgrade.isNone) {
+            console.log(" No authorized upgrade found");
             return true;
         }
-    } else {
-        console.log(" No authorized upgrade found");
-        if (expectedCodeHash) {
-            throw new Error("Expected authorized upgrade with hash: " + expectedCodeHash + " but none found");
-        }
-        return true;
+
+        // else sleep and retry
+        await new Promise((resolve) => setTimeout(resolve, 6000));
     }
 }
 
